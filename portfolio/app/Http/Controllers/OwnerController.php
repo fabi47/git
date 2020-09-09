bf22f463764ab07367979c6ed9fe0d1e071b36a1
SouzaFabiano <67329184+fabi47@users.noreply.github.com>
2020-09-09 12:53:34 +0900

    Update OwnerController.php

--- portfolio/app/Http/Controllers/OwnerController.php
+++ portfolio/app/Http/Controllers/OwnerController.php
@@ -8,17 +8,7 @@ class OwnerController extends Controller
     public function index (Request $request)
     {
         $all_inputs = $requst->all();
-      //$items = Owner::all();
-
-        $owner = new \App\Owner;
-
-        $owner->name = $request->owner;
-        $owner->tel = $request->owner;
-        $owner->car_name = $request->owner;
-        $owner->car_number = $request->owner;
-        $owner->save();
 
         return view ('owner.index', compact('owners'));
-        //return redirect('/form/index');
     }
 }
