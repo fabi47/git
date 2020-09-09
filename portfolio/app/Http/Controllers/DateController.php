99b8a97820bbefcb1ffde7acdb523fc65f37292f
SouzaFabiano <67329184+fabi47@users.noreply.github.com>
2020-09-09 12:51:43 +0900

    Update DateController.php

--- portfolio/app/Http/Controllers/DateController.php
+++ portfolio/app/Http/Controllers/DateController.php
@@ -1,13 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 
 class DateController extends Controller
 {
-    public function index ()
+    public function index (Request $request)
     {
-        return view('date');
+        $owner = new \App\Owner;
+
+        $owner->name = $request['name'];
+        $owner->updated_at = "2020-09-09 12:00:00";
+        $owner->created_at = "2020-09-09 12:00:00";
+        $owner->car_name = $request['car_name'];  
+        $owner->save();
+        
+        return view('date', compact('owner'));
     }
 }
