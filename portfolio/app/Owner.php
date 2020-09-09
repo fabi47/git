f564aa28a4f1fe6481115de6a574d35a36a87299
SouzaFabiano <67329184+fabi47@users.noreply.github.com>
2020-09-09 12:48:55 +0900

    Update Owner.php

--- portfolio/app/Owner.php
+++ portfolio/app/Owner.php
@@ -4,10 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Owner extends Model
 {
-
-  protected $touches = ['post'];
-    // Relationship
     public function post() {
-        return $this->belongsTo('App\Post');
     }
 }
