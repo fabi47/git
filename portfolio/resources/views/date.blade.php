d3969c9e73ffa4b799d75d1ab950566566112991
SouzaFabiano <67329184+fabi47@users.noreply.github.com>
2020-09-15 13:35:19 +0900

    Update date.blade.php

--- portfolio/resources/views/date.blade.php
+++ portfolio/resources/views/date.blade.php
@@ -5,19 +5,36 @@
         <title>daet</title>
         <link rel="stylesheet" href="date.css" />
         <link rel="stylesheet" href="all.css" />
-      </head>
+        <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.3.2/main.css' rel='stylesheet' />
+        <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.3.2/main.min.js'></script>
+        <script>
+        
+            document.addEventListener('DOMContentLoaded', function() {
+                var calendarEl = document.getElementById('calendar');
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                    initialView: 'timeGridWeek',
+                    selectable: true,
+                    dateClick: function(info) {
+                        document.getElementById('date_start').value = info.dateStr;
+                    }
+                });
+                calendar.render();
+            });
+
+        </script>
+    </head>
     <body>
         <header>header</header>
-            <main>
-                <img src="date_back.jpg" title="date_back">
-                <div class="date">
-                    <P>ここにカレンダー</p>
-                </div>
+        <main>
+            <div id='calendar'></div>
+            <div class="date">
                 <form action="/check" method="post" enctype="multipart/form-data">
-                @csrf
-                <input type="submit" value="next">
-              </form>
-            </main>
+                    @csrf
+                    <input type="hidden" name="date_start" value="" id="date_start" name="date_start">
+                    <input type="submit" value="next">
+                </form>
+            </div>
+        </main>
         <footer>footer</footer>
     </body>
 </html>
